50cb8a395a414a99048d5e3813b763a6365dc70d
Haris123rrl <hzhshsjsjajaja@gmail.com>
2023-08-03 02:56:29 +0700

    bebas

---
+++ config.js
@@ -0,0 +1,60 @@
+/**
+    * Created By AdrianTzy
+    * Mau Recode? Simpen Nama Gw!!!!
+    * Hapus Credit Masuk Neraka Paling Bawah
+*/
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Gak Usah Di Ganti
+global.apikey = '3d27cc6592c983dd253f642f' // https://api.lolhuman.xyz
+global.rosekey = 'cba86fac49e37d63f9bd4561' // https://api.itsrose.site
+
+//—————「 Set Nama Bot & Own 」—————//
+global.namabot = 'MyokuBotz-MD'
+global.namaowner = 'haris'
+
+//—————「 Setting Owner 」—————//
+global.owner = '6285936162097'
+global.nomerowner = ["6285936162097"]
+global.premium = ['6288216922399']
+
+//—————「 Set Wm 」—————//
+global.packname = 'Myokubotz'
+global.author = 'V1.2'
+global.isLink = 'https://chat.whatsapp.com/CTOLdac0BmFCuIokIxqoIZ'
+global.prefa = ['', '.']
+global.sp = '•'
+
+//—————「 Set Message 」—————//
+global.mess = {
+    success: '🤗Done, Oke Desu~',
+    admin: '❗Perintah Ini Hanya Bisa Digunakan Oleh Admin Group !',
+    botAdmin: '❗Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin Group !',
+    owner: '❗Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
+    group: '❗Perintah Ini Hanya Bisa Digunakan Di Group Chat !',
+    private: '❗Perintah Ini Hanya Bisa Digunakan Di Private Chat !',
+    bot: '🤖 Fitur Khusus Pengguna Nomor Bot !',
+    wait: '⏳ Sedang Di Proses !',
+    endLimit: '🕊️ Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12 !',
+    error: '🚫 Fitur Sedang Error !',
+}
+
+//—————「 Set Limit 」—————//
+global.limitawal = {
+    premium: "Infinity",
+    free: 50,
+}
+
+//—————「 Set Image 」—————//
+global.imageurl = 'https://telegra.ph/file/17c8e34c33011d418b13c.jpg'
+global.thumb = fs.readFileSync('./media/thumb.jpg')
+
+//—————「 Batas Akhir 」—————//
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+    fs.unwatchFile(file)
+    console.log(chalk.redBright(`Update'${__filename}'`))
+    delete require.cache[file]
+    require(file)
+})
